b454fdd9cf2118b28dec190839273220503a161f
theandersonn <theandersonn@gmail.com>
2022-04-04 21:46:58 -0300

    refactor: pastas mongodb

--- src/mongodb/scripts/add.ts
+++ src/mongodb/scripts/add.ts
@@ -1,18 +1,22 @@
-import { MongoHelper } from './helpers/mongo-helper';
-import env from './config/env';
+import { MongoHelper } from '../helpers/mongo-helper';
+import env from '../config/env';
 
-import add from './scripts/add';
-
+// database connection
 MongoHelper.connect(env.mongoUrl)
   .then(() => console.log(`🚀 mongodb is connected`))
   .catch(() => console.log('Error String to connect'));
 
-// criar item na coleção
+// item
 const quoteItem = {
   description: "Errei mais de nove mil arremessos na minha carreira. Perdi quase 300 jogos. Vinte e seis vezes fui escolhido para fazer o arremesso final e falhei. Falhei vezes e mais vezes na minha carreira. E foi por causa disso que me tornei um VENCEDOR!",
   author: "Michael Jordan",
   createdAt: new Date(),
   updatedAt: new Date()
 }
+
+const add = async (item: object, collection: string): Promise<void> => {
+  const quoteCollection = await MongoHelper.getCollection(collection);
+  await quoteCollection.insertOne(item);
+};
 
 add(quoteItem, 'quotes');
--- src/scripts/add.ts
+++
@@ -1,8 +0,0 @@
-import { MongoHelper } from '../helpers/mongo-helper';
-
-const add = async (item: object, collection: string): Promise<void> => {
-  const quoteCollection = await MongoHelper.getCollection(collection);
-  await quoteCollection.insertOne(item);
-};
-
-export default add;
